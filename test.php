2174561eea390b6ca22b9984f46afb084a084c32
Nikolay <dazzlo2292@mail.ru>
2022-12-06 14:42:04 +0300

    add validator

---
+++ test.php
@@ -0,0 +1,22 @@
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Validation;
+
+<?php
+
+    echo "Hello from PHP";
+
+    $validator = Validation::createValidator();
+    $violations = $validator->validate('Bernhard', [
+        new Length(['min' => 10]),
+        new NotBlank(),
+    ]);
+    
+    if (0 !== count($violations)) {
+        // there are errors, now you can show them
+        foreach ($violations as $violation) {
+            echo $violation->getMessage().'<br>';
+        }
+    }
